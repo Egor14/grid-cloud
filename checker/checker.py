93f02dcbb6ce3e7cc6fb2267e735059b35ff8355
Egor14 <egorgrekhnev@mail.ru>
2019-12-14 18:51:15 +0300

    correct azure commands

--- checker/checker.py
+++ checker/checker.py
@@ -8,16 +8,16 @@ django.setup()
 from cloudtask import settings
 
 from worker.models import Task
-import subprocess
 import time
 
 while True:
     if len(Task.objects.all()) >= 5:
-        subprocess.run(f'az login -u {settings.azure_username} -p {settings.azure_password}')
-        subprocess.run('az vm start --name MyVm1 --resource-group MyResourceGroup')
+        os.system(f'az login -u {settings.azure_username} -p {settings.azure_password}')
+        os.system('az vm start --name MyVm1 --resource-group MyResourceGroup')
         while(len(Task.objects.all()) > 0):
             time.sleep(5)
         else:
-            subprocess.run('az vm stop --resource-group MyResourceGroup --name MyVm1')
+            os.system(f'az login -u {settings.azure_username} -p {settings.azure_password}')
+            os.system('az vm stop --resource-group MyResourceGroup --name MyVm1')
 
     time.sleep(60)
